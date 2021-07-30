69c8c2ff72cd322e3a92d5f6692144a1204fc300
Mikele91 <michele.amoruso91.8@gmail.com>
2021-07-30 11:02:16 +0200

    aggiutata punteggiatura

--- script/script.js
+++ script/script.js
@@ -6,21 +6,31 @@ console.log(km);
 console.log(eta);
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
-var prezzo = km * prezzoKm;
-console.log( prezzo);
-document.getElementById("prezzo1").innerHTML += " " + prezzo.toFixed(2) + "€"
 
-// va applicato uno sconto del 20% per i minorenni
-if ( eta < 18 ){
-   prezzo -= (prezzo * 20) / 100 
-    document.getElementById("sconto").innerHTML += " Lo sconto sarebbe del 20%  Ma tua madre ci ha chiamato e TI PROIBISCE DI PARTIRE "
+if(!isNaN(km) && !isNaN(eta)){
 
-} else if (eta >= 65){
-    prezzo -= (prezzo * 40) / 100 
-    document.getElementById("sconto").innerHTML = " Hai più di 65 anni lo sconto è del  40% ma noi ti consigliamo di stare a casa!!!"
 
-}
-console.log (prezzo.toFixed(2));
-document.getElementById("totale").innerHTML = " " + prezzo.toFixed(2) + "€"
-// va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+    var prezzo = km * prezzoKm;
+    console.log( prezzo);
+    document.getElementById("prezzo1").innerHTML += " " + prezzo.toFixed(2) + "€";
+
+    // va applicato uno sconto del 20% per i minorenni
+    if ( eta < 18 ){
+        prezzo -= (prezzo * 20) / 100 ;
+        document.getElementById("sconto").innerHTML += " Lo sconto sarebbe del 20%  Ma tua madre ci ha chiamato e TI PROIBISCE DI PARTIRE ";
+
+    } else if (eta >= 65){
+        prezzo -= (prezzo * 40) / 100;
+        document.getElementById("sconto").innerHTML = " Hai più di 65 anni lo sconto è del  40% ma noi ti consigliamo di stare a casa!!!";
+
+    }
+    
+    console.log (prezzo.toFixed(2));
+    document.getElementById("totale").innerHTML = " " + prezzo.toFixed(2) + "€";
+    // va applicato uno sconto del 40% per gli over 65.
+    // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali 
+
+} else {
+ alert( "Dati non validi!!!!");
+}     
